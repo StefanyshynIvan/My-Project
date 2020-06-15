13bcbe2880adfc7b861470bf55262512954adf65
Ivan Sefanyshyn <vano240696@gmail.com>
2020-06-15 23:11:20 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,81 @@
+//reverseArray
+let arr = [1, 2, 5, 3, 4],
+    arr2 = [1, 1, 1, 1];
+// arr.reverse(arr);
+
+// function printReverseArr(arr){
+//     let newArr = [];
+// for(let i = arr.length -1; i >= 0; i --){
+    
+//     newArr.push(arr[i]);
+// }
+// console.log(newArr);
+// }
+// printReverseArr(arr);
+
+//uniform
+
+// function compValue(elem) {
+//     let first = elem[0];
+//     for(let i = 1; i < elem.length; i +=1 ){
+//         if(elem[i] !== first){
+//             console.log(false);
+//             return false;
+//         }
+//         console.log(true);
+//         return true;
+//     }
+// }
+// compValue(arr2);
+
+// sum OF ARRAY
+
+// function sumArray(elem) {
+//     let result = 0;
+//     for(let i = 0; i < elem.length; i +=1 ){
+//         result += elem[i];
+        
+//     }
+//     console.log(result);
+//     return result;
+// }
+// sumArray(arr);
+
+
+// function totalArray(arr) {
+//     let result = 0;
+//     arr.forEach(function(elem){
+//         result += elem;
+        
+//     });
+//     console.log(result);
+//     return result; 
+// }
+
+// totalArray(arr);
+
+//max mumber in array
+
+// function maxNum(arr){
+//     let max = arr[0];
+//     for(let i = 1; i < arr.length; i+=1){
+//         if(arr[i] > max){
+//             max = arr[i];
+//         }    
+//     }
+//     console.log(max);
+// }
+
+// maxNum(arr);
+
+// function maxNumInArray(arr){
+//     let max = arr[0];
+//     arr.forEach(function(elem){
+//         if(elem > max){
+//             max = elem;
+//         }
+//     });
+//     console.log(max);
+// }
+
+// maxNumInArray(arr);
